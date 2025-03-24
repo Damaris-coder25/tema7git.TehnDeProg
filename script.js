6f9e4684284f2bb6bfa256d59ce9cd4ac958d0c3
Damaris <damaris.ionel@emanuel.ro>
2025-03-24 17:32:48 +0200

    add the sun

--- script.js
+++ script.js
@@ -5,6 +5,15 @@ function setup() {
 function draw() {
     background("#eeeeee")
 
+//the sun
 fill("yellow")
-circle(500,250,40)
+circle(500,250,50)
+triangle(490, 220, 510, 220, 500, 195)
+triangle(490, 280, 510, 280, 500, 305)
+triangle(470, 260, 470, 240, 445, 250)
+triangle(530, 260, 530, 240, 555, 250)
+triangle(515, 220, 528, 235, 540, 210)
+triangle(515, 280, 528, 265, 540, 290)
+triangle(485, 220, 470, 235, 460, 210)
+triangle(485, 280, 470, 265, 460, 290)
 }
