94b8a4d11b2c1fd7ff86c1b379dad21e43309e58
Damaris <damaris.ionel@emanuel.ro>
2025-03-24 18:30:08 +0200

    add planets

--- script.js
+++ script.js
@@ -17,13 +17,32 @@ triangle(515, 280, 528, 265, 540, 290)
 triangle(485, 220, 470, 235, 460, 210)
 triangle(485, 280, 470, 265, 460, 290)
 
-//the orbits
+//orbits
 noFill()
 stroke("black"); 
-ellipse(500, 250, 950, 340)
-ellipse(500, 250, 800, 320)
-ellipse(500, 250, 650, 300)
-ellipse(500, 250, 500, 270)
-ellipse(500, 250, 400, 240)
+ellipse(500, 250, 950, 360)
+ellipse(500, 250, 730, 340)
+ellipse(500, 250, 570, 320)
+ellipse(500, 250, 420, 290)
+ellipse(500, 250, 320, 260)
 
+//planets
+fill(148, 191, 152)
+circle(900, 340, 90)
+  fill(0); 
+  textAlign(CENTER, CENTER); 
+  textSize(20); 
+  text("P5", 900, 340);
+fill(180, 191, 218)
+circle(830, 185, 65)
+  fill(0); 
+  textAlign(CENTER, CENTER); 
+  textSize(15); 
+  text("P4", 830, 185);
+fill(224, 195, 239)
+circle(240, 190, 50)
+fill(246, 197, 246)
+circle(295, 240, 35)
+fill(166, 247, 247)
+circle(375, 170, 20)
 }
